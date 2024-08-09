104219bb28659d120a5c4f3f8812df4492674f97
adrianfwd <azuniga@fwdcostarica.com>
2024-08-09 06:29:19 -0600

    sa

--- process.js
+++ process.js
@@ -1,14 +1,11 @@
 let tablero;
 const jugadorUno = "O";
 const cpu = "X";
-let scoreJug = 0;
-let scoreCpu = 0;
-let empate = 0;
 let scores = {
-   scoreCpu: 0,
-   scoreJug: 0,
-   empate :0
-}
+    scoreCpu: 0,
+    scoreJug: 0,
+    empate: 0
+};
 const lineas = [
     [0, 1, 2],
     [3, 4, 5],
@@ -23,27 +20,28 @@ const celdas = document.querySelectorAll(".celdas");
 empezarLaVara();
 
 function empezarLaVara() {
-
     tablero = [];
-    for (var i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
         tablero[i] = i;
     }
 
-
-    for (var j = 0; j < celdas.length; j++) {
+    for (let j = 0; j < celdas.length; j++) {
         celdas[j].innerText = "";
         celdas[j].style.backgroundColor = "";
         celdas[j].addEventListener("click", turnoClick);
     }
 
     document.querySelector(".ventana .text").innerText = "";
-   
 }
 
 function turnoClick(cuadro) {
     if (typeof tablero[cuadro.target.id] == "number") {
         turno(cuadro.target.id, jugadorUno);
-        if (!revisarEmpate() && !checkWin(tablero, jugadorUno)) turno(mejorCuadro(), cpu);
+        if (!revisarEmpate() && !checkWin(tablero, jugadorUno)) {
+            setTimeout(function () {
+                turno(mejorCuadro(), cpu);
+            }, 500);
+        }
     }
 }
 
@@ -66,7 +64,14 @@ function checkWin(tablero, jugador) {
         var linea = lineas[i];
         var esGanador = true;
         for (var j = 0; j < linea.length; j++) {
-            if (jugadas.indexOf(linea[j]) === -1) {
+            var encontrado = false;
+            for (var k = 0; k < jugadas.length; k++) {
+                if (jugadas[k] === linea[j]) {
+                    encontrado = true;
+                    break;
+                }
+            }
+            if (!encontrado) {
                 esGanador = false;
                 break;
             }
@@ -75,14 +80,12 @@ function checkWin(tablero, jugador) {
             return { index: i, jugador: jugador };
         }
     }
-
     return null;
 }
 
 function derrota(victoria) {
-    var indices = lineas[victoria.index]; 
+    var indices = lineas[victoria.index];
     var color;
-
     if (victoria.jugador == jugadorUno) {
         color = "blue";
     } else {
@@ -90,25 +93,23 @@ function derrota(victoria) {
     }
 
     for (var i = 0; i < indices.length; i++) {
-        var index = indices[i];
-        document.getElementById(index).style.backgroundColor = color;
+        document.getElementById(indices[i]).style.backgroundColor = color;
     }
 
     if (victoria.jugador == jugadorUno) {
         scores.scoreJug++;
-        console.log("Jugador: " + scoreJug);
+        console.log("Jugador: " + scores.scoreJug);
     } else {
         scores.scoreCpu++;
-        console.log("CPU: " + scoreCpu);
+        console.log("CPU: " + scores.scoreCpu);
     }
 
     for (var i = 0; i < celdas.length; i++) {
         celdas[i].removeEventListener('click', turnoClick);
     }
 
-    // Mostrar el resultado esto es solo para debuggear tengo que hacer una ventana que imprima estos datos jeje
     guardarScore();
-    declararGanador(victoria.jugador == jugadorUno ? "ganaste" : "te verguearon que pato");
+    declararGanador(victoria.jugador == jugadorUno ? "¡Ganaste!" : "¡Te ganaron!");
 }
 
 function declararGanador(mensaje) {
@@ -116,50 +117,39 @@ function declararGanador(mensaje) {
     document.querySelector(".ventana .text").innerText = mensaje;
 }
 
- var rango = 0;
 function espacioVacio() {
     var espacios = [];
     for (var i = 0; i < tablero.length; i++) {
         if (typeof tablero[i] == "number") {
             espacios.push(tablero[i]);
-            //necesito verificar cuantos numeros hay en la tabla para que el random pueda funcionar bien acordarse de crear la variable que vaya junto a los numeros en el tablero
-
         }
     }
-    var rango = (espacios.length) - 1;
-    console.log(rango);//estoy debugueando los espacios preguntarle a nicol porque agrega un espacio de mas(prioridad)
     return espacios;
-    
 }
 
 function mejorCuadro() {
     var vacios = espacioVacio();
-    return vacios[getRandomInt(rango)];
-    // cambiar este metodo a un random o minimax si sobra tiempo
+    return vacios[getRandomInt(vacios.length)];
 }
 
-function revisarEmpate(){
-    
+function revisarEmpate() {
     if (espacioVacio().length == 0) {
         for (var i = 0; i < celdas.length; i++) {
             celdas[i].style.backgroundColor = "green";
             celdas[i].removeEventListener("click", turnoClick);
         }
-        declararGanador("Empate");
-        scores.empate++
-        console.log("empate: " + empate);
+        declararGanador("¡Empate!");
+        scores.empate++;
         guardarScore();
         return true;
-        
     }
     return false;
 }
 
-function guardarScore(){
-
+function guardarScore() {
     localStorage.setItem("scores", JSON.stringify(scores));
 }
 
-function getRandomInt(rango) {
-    return Math.floor(Math.random() * rango);
-  }
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
